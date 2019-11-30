b260098c9fe8f0f799727fdb13cfdab4f76bf219
shamor <yzban@mail.ahnu.edu.cn>
2019-11-30 21:18:59 +0800

    clear code

--- snaker-core/src/main/java/org/snaker/engine/handlers/impl/CreateTaskHandler.java
+++ snaker-core/src/main/java/org/snaker/engine/handlers/impl/CreateTaskHandler.java
@@ -1,17 +1,4 @@
-/* Copyright 2013-2015 www.snakerflow.com.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.snaker.engine.handlers.impl;
 
 import java.util.List;
@@ -28,6 +15,7 @@ import org.snaker.engine.model.TaskModel;
 
 /**
  * 任务创建操作的处理器
+ * 
  * @author yuqs
  * @since 1.0
  */
@@ -37,15 +25,16 @@ public class CreateTaskHandler implements IHandler {
 	 * 任务模型
 	 */
 	private TaskModel model;
-	
+
 	/**
 	 * 调用者需要提供任务模型
+	 * 
 	 * @param model 模型
 	 */
 	public CreateTaskHandler(TaskModel model) {
 		this.model = model;
 	}
-	
+
 	/**
 	 * 根据任务模型、执行对象，创建下一个任务，并添加到execution对象的tasks集合中
 	 */
@@ -57,10 +46,10 @@ public class CreateTaskHandler implements IHandler {
 		 */
 		List<SnakerInterceptor> interceptors = ServiceContext.getContext().findList(SnakerInterceptor.class);
 		try {
-			for(SnakerInterceptor interceptor : interceptors) {
+			for (SnakerInterceptor interceptor : interceptors) {
 				interceptor.intercept(execution);
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			log.error("拦截器执行失败=" + e.getMessage());
 			throw new SnakerException(e);
 		}
